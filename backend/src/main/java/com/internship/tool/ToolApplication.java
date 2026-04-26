3270e9a86cce617aface7d0ab83e6046d807fd5b
Raksha V Kadagi <rkadagi9@gmail.com>
2026-04-26 22:20:53 +0530

    Day 1 completed : Spring Boot backend setup and React frontend initialized

---
+++ backend/src/main/java/com/internship/tool/ToolApplication.java
@@ -0,0 +1,14 @@
+package com.internship.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolApplication {
+
+    public static void main(String[] args) {
+
+        // Starts Spring Boot application
+        SpringApplication.run(ToolApplication.class, args);
+    }
+}
